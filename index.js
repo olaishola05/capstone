a7ceefca78aefd8bfa1d4f968d7c4eaf849c5e96
Ola Ishola <45001916+netman5@users.noreply.github.com>
2021-12-21 23:23:31 +0100

    Add dynamic mobile menu && hide logo on mobile menu

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-console.log("We are happy!");
+const menu = document.querySelector(".hamburger");
+const showMenu = document.querySelector(".bottom-nav");
+const logo = document.querySelector(".logo");
+const closeBtn = document.querySelector(".close-btn");
+
+const toggleOpen = false;
+const toggleClose = false;
+
+const toggleNavigation = () => {
+  if (toggleOpen === false) {
+    showMenu.style.visibility = "visible";
+  }
+};
+
+const closeNavigation = () => {
+  if (toggleClose === false) {
+    showMenu.style.visibility = "hidden";
+  }
+};
+
+const navs = document.querySelectorAll(".bottom-nav li");
+navs.forEach((link) => {
+  link.addEventListener("click", () => {
+    showMenu.style.visibility = "hidden";
+  });
+});
+
+menu.addEventListener("click", toggleNavigation);
+closeBtn.addEventListener("click", closeNavigation);
