84fa24bbd7dcad919773f1e57078362f154e0335
Ola Ishola <45001916+netman5@users.noreply.github.com>
2021-12-22 11:13:57 +0100

    Add speaker dynamically using JS && restyled mobile program section

--- public/JS/feature_speakers.js
+++ public/JS/feature_speakers.js
@@ -1,33 +1,29 @@
-import { speakers } from "./speakers.js";
+import { speakers } from './speakers.js';
 
-const speakerContainer = document.getElementById("speakers");
-let speakerItems = "";
+const speakerContainer = document.getElementById('speakers');
+let speakerItems = '';
 for (let i = 0; i < speakers.length; i += 1) {
-  console.log(speakers[i]);
-  speakerItems = `<div class="speaker">
+  speakerItems += `<div class="speaker">
           <img
             src="${speakers[i].checkerUrl}"
-            alt=""
+            alt="checker background"
             class="checkers"
           />
           <img
             src="${speakers[i].imageUrl}"
-            alt="Bishop David Oyedepo"
+            alt="${speakers[i].name} featured speaker""
             class="speaker-image"
           />
           <div class="name-professional">
             <h4>${speakers[i].name}</h4>
             <p class="description">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Doloribus, aliquid.
+              ${speakers[i].title}
             </p>
             <hr />
             <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Veritatis, quidem.
+              ${speakers[i].specialty}
             </p>
           </div>
         </div>`;
+  speakerContainer.innerHTML = speakerItems;
 }
-
-speakerContainer.innerHTML = speakerItems;
--- public/JS/speakers.js
+++ public/JS/speakers.js
@@ -1,62 +1,60 @@
 // eslint-disable-next-line import/prefer-default-export
 export const speakers = [
   {
-    name: "Bishop David Oyedope",
+    name: 'Bishop David Oyedope',
     title:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus, aliquid.",
+      'David O. Oyedepo is a Nigerian preacher, Christian author, businessman, architect and founder.',
     specialty:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis, quidem.",
-    imageUrl: "./public/images/papa.jpeg",
-    checkerUrl: "./public/images/checkers-img-holder.jpeg",
+      "Founder & Presiding Bishop of the megachurch Faith Tabernacle in Ota, Ogun State, Nigeria, and Living Faith Church Worldwide, also known as Winners' Chapel International",
+    imageUrl: './public/images/papa.jpeg',
+    checkerUrl: './public/images/checkers-img-holder.jpeg',
   },
 
   {
-    name: "Bishop David Abioye",
-    title:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus, aliquid.",
+    name: 'Bishop David Abioye',
+    title: 'David Olatunji Abioye is a Nigerian Christian author and preacher.',
     specialty:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis, quidem.",
-    imageUrl: "./public/images/abioye.jpeg",
-    checkerUrl: "./public/images/checkers-img-holder.jpeg",
+      'The First Vice President of the Living Faith Church WorldwideHe is a bishop and the senior pastor of the Abuja church with over 30,000 worshippers.',
+    imageUrl: './public/images/abioye.jpeg',
+    checkerUrl: './public/images/checkers-img-holder.jpeg',
   },
 
   {
-    name: "Pastor Dr. Eneche",
+    name: 'Pastor Dr. Eneche',
     title:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus, aliquid.",
+      'Dr Pastor Paul Enenche is a trained Medical Doctor & founder, called of God into full time ministry',
     specialty:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis, quidem.",
-    imageUrl: "./public/images/eneche.jpeg",
-    checkerUrl: "./public/images/checkers-img-holder.jpeg",
+      'His ministrations are characterized by deep undiluted insights from the Word of God, accurate Word of Knowledge and the demonstration of the power of God in Supernatural healings, miracles, signs and wonders.',
+    imageUrl: './public/images/eneche.jpeg',
+    checkerUrl: './public/images/checkers-img-holder.jpeg',
   },
 
   {
-    name: "Pastor David Ibiyome",
+    name: 'Pastor David Ibiyome',
     title:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus, aliquid.",
+      'David Ibiyeomie is a Nigerian pastor, author, televangelist and founder.',
     specialty:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis, quidem.",
-    imageUrl: "./public/images/ibiyome.jpeg",
-    checkerUrl: "./public/images/checkers-img-holder.jpeg",
+      'Through his television ministry Hour of Salvation and live services, David Ibiyeomie reaches millions of worshippers. He is an author and hosts an internet radio show.',
+    imageUrl: './public/images/ibiyome.jpeg',
+    checkerUrl: './public/images/checkers-img-holder.jpeg',
   },
 
   {
-    name: "Dr. Kenneth Copeland",
-    title:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus, aliquid.",
+    name: 'Dr. Kenneth Copeland',
+    title: 'Kenneth Max Copeland is an American televangelist and author.',
     specialty:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis, quidem.",
-    imageUrl: "./public/images/copeland.jpeg",
-    checkerUrl: "./public/images/checkers-img-holder.jpeg",
+      "The organization he founded in 1967, Eagle Mountain International Church Inc. is based in Tarrant County, Texas. Copeland's sermons are broadcast across the US and worldwide on the Victory Channel.",
+    imageUrl: './public/images/copeland.jpeg',
+    checkerUrl: './public/images/checkers-img-holder.jpeg',
   },
 
   {
-    name: "Bishop Thomas Aremu",
+    name: 'Bishop Thomas Aremu',
     title:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus, aliquid.",
+      'Bishop Thomas Aremu is the Vice President (Foreign Missions) of the Living Faith Church Worldwide (Winners Chapel)',
     specialty:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis, quidem.",
-    imageUrl: "./public/images/aremu.jpeg",
-    checkerUrl: "./public/images/checkers-img-holder.jpeg",
+      'He presently occupies the position of a Missioner of Living Faith Church Worldwide ',
+    imageUrl: './public/images/aremu.jpeg',
+    checkerUrl: './public/images/checkers-img-holder.jpeg',
   },
 ];
