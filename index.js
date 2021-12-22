482cf414bb2bf5cd300c3c4a07bc64c0b9f43715
Ola Ishola <45001916+netman5@users.noreply.github.com>
2021-12-22 19:25:45 +0100

    Update readme, removed unused code and comments, hide partner section on mobile

--- index.js
+++ index.js
@@ -1,28 +1,21 @@
-const menu = document.querySelector(".hamburger");
-const showMenu = document.querySelector(".bottom-nav");
-const closeBtn = document.querySelector(".close-btn");
+const menu = document.querySelector('.hamburger');
+const showMenu = document.querySelector('.bottom-nav');
+const closeBtn = document.querySelector('.close-btn');
 
 const toggleOpen = false;
 const toggleClose = false;
 
 const toggleNavigation = () => {
   if (toggleOpen === false) {
-    showMenu.style.visibility = "visible";
+    showMenu.style.visibility = 'visible';
   }
 };
 
 const closeNavigation = () => {
   if (toggleClose === false) {
-    showMenu.style.visibility = "hidden";
+    showMenu.style.visibility = 'hidden';
   }
 };
 
-const navs = document.querySelectorAll(".bottom-nav li");
-navs.forEach((link) => {
-  link.addEventListener("click", () => {
-    showMenu.style.visibility = "hidden";
-  });
-});
-
-menu.addEventListener("click", toggleNavigation);
-closeBtn.addEventListener("click", closeNavigation);
+menu.addEventListener('click', toggleNavigation);
+closeBtn.addEventListener('click', closeNavigation);
